a3d75d02ad0730e9aea8cd28abd73ab453061c34
Andrii <148819392+andriikamanin@users.noreply.github.com>
2025-01-17 17:49:57 +0100

    реализован базовый класс для транзакции

--- core/src/main/java/it/volta/ts/kamaninandrii/blockchain/Transaction.java
+++ core/src/main/java/it/volta/ts/kamaninandrii/blockchain/Transaction.java
@@ -1,4 +1,43 @@
 package it.volta.ts.kamaninandrii.blockchain;
 
+import java.util.Date;
+
 public class Transaction {
+    private String sender;
+    private String receiver;
+    private double amount;
+    private long timestamp;
+
+    public Transaction(String sender, String receiver, double amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.timestamp = new Date().getTime();
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                ", amount=" + amount +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
 }
