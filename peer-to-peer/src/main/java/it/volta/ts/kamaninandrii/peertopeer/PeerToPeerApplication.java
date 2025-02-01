faa6a2f727ac03bf70ffad637c86dfb167b84a42
Andrii <148819392+andriikamanin@users.noreply.github.com>
2025-02-01 14:04:05 +0100

    добавление peer-to-peer соединения для блокчейна

---
+++ peer-to-peer/src/main/java/it/volta/ts/kamaninandrii/peertopeer/PeerToPeerApplication.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.peertopeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerToPeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerToPeerApplication.class, args);
+    }
+
+}
---
+++ peer-to-peer/src/test/java/it/volta/ts/kamaninandrii/peertopeer/PeerToPeerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.peertopeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerToPeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
